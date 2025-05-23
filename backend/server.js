c13486bd0c4baabed449885e9a62340fa33cae42
Rahul <rpawar141199@gmail.com>
2025-05-23 17:44:08 +0530

    Fix: use PORT properly for Render

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-
 const cardRoutes = require("./routes/cardRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -19,8 +17,8 @@ app.use("/api/cards", cardRoutes);
 mongoose.connect("mongodb+srv://rpawar141199:xm7qbYvf8NkMoreK@cluster0cardcraft.ueg9v7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0CardCraft")
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error("Connection error:", err));
